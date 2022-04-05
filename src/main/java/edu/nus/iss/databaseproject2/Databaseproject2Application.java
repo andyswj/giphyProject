97f6e138eae8e5b2e9cbaef6d608881d54b60ebd
andyswj <e0696682@u.nus.edu>
2022-04-05 11:07:25 +0800

    initial commit

---
+++ src/main/java/edu/nus/iss/databaseproject2/Databaseproject2Application.java
@@ -0,0 +1,13 @@
+package edu.nus.iss.databaseproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Databaseproject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Databaseproject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nus/iss/databaseproject2/Databaseproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nus.iss.databaseproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Databaseproject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
